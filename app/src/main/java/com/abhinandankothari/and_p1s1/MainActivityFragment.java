b4827de14fc7885a313df65a5027710ca1f5e67d
Abhinandan Kothari <abhinandankothari@gmail.com>
2016-02-13 20:18:12 +0530

    Added Picasso

--- app/src/main/java/com/abhinandankothari/and_p1s1/MainActivityFragment.java
+++ app/src/main/java/com/abhinandankothari/and_p1s1/MainActivityFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +20,9 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ImageView imageView = (ImageView)rootView.findViewById(R.id.image_main);
+        Picasso.with(getActivity().getApplicationContext()).load("http://i.imgur.com/DvpvklR.png").into(imageView);
+        return rootView;
     }
 }
